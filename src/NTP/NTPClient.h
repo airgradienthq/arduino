1ea4a05d5af79a4d1c840928e8ba6ab2a9ffd4a6
Antoine Aflalo <Belphemur@users.noreply.github.com>
2022-02-03 18:43:59 -0500

    Add documentation

--- src/NTP/NTPClient.h
+++ src/NTP/NTPClient.h
@@ -1,9 +1,11 @@
 #pragma once
+
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 
 #include <utility>
+
 #define NTP_PACKET_SIZE 48
 #define SEVENTIES 2208988800UL
 #define NTP_DEFAULT_LOCAL_PORT 1515
@@ -20,18 +22,26 @@ namespace AirGradient {
     class NTPClient {
 
     public:
-        NTPClient() {
-            _udp.begin(NTP_DEFAULT_LOCAL_PORT);
-        }
+
+        /**
+         * Get current Time in UTC in UnixEpoch
+         * @return
+         */
         time_t getUtcUnixEpoch();
+
         virtual ~NTPClient() = default;
 
         NTPClient(String ntpServer);
 
     private:
+        NTPClient() {
+            _udp.begin(NTP_DEFAULT_LOCAL_PORT);
+        }
+
         String _ntpServer;
         WiFiUDP _udp;
         byte _packetBuffer[NTP_PACKET_SIZE]{};
+
         void sendNTPpacket(IPAddress &address);
 
     };
