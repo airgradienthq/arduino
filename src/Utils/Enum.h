e49618b2619a31b3130a859068e24597189cac5d
Antoine Aflalo <Belphemur@users.noreply.github.com>
2022-02-03 21:40:14 -0500

    Overload ! operator
    To check if the flag is valid, useful when checking if a flag is present in given flags

--- src/Utils/Enum.h
+++ src/Utils/Enum.h
@@ -9,4 +9,5 @@
     inline T operator^ (T a, T b) { return static_cast<T>( static_cast<std::underlying_type<T>::type>(a) ^ static_cast<std::underlying_type<T>::type>(b) ); }                   \
     inline T& operator|= (T& a, T b) { return reinterpret_cast<T&>( reinterpret_cast<std::underlying_type<T>::type&>(a) |= static_cast<std::underlying_type<T>::type>(b) ); }   \
     inline T& operator&= (T& a, T b) { return reinterpret_cast<T&>( reinterpret_cast<std::underlying_type<T>::type&>(a) &= static_cast<std::underlying_type<T>::type>(b) ); }   \
-    inline T& operator^= (T& a, T b) { return reinterpret_cast<T&>( reinterpret_cast<std::underlying_type<T>::type&>(a) ^= static_cast<std::underlying_type<T>::type>(b) ); }
+    inline T& operator^= (T& a, T b) { return reinterpret_cast<T&>( reinterpret_cast<std::underlying_type<T>::type&>(a) ^= static_cast<std::underlying_type<T>::type>(b) ); } \
+    inline bool operator! (T a) { return static_cast<std::underlying_type<T>::type>(a) != 0;}
