22005cf661b57281cd022036c8e38d24a698ba8b
Antoine Aflalo <Belphemur@users.noreply.github.com>
2022-02-04 20:48:18 -0500

    Add MHZ19Sensor

---
+++ src/Sensors/CO2/MHZ19Sensor.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Antoine on 2022-02-04.
+//
+
+#include "MHZ19Sensor.h"
+
+bool AirGradient::MHZ19Sensor::begin() {
+    _softwareSerial = std::make_unique<SoftwareSerial>(_rxPin, _txPin);
+    _softwareSerial->begin(_baudRate);
+    _sensor = std::make_unique<MHZ19>();
+    _sensor->begin(*_softwareSerial);
+    _sensor->autoCalibration();
+
+    return _sensor->errorCode == RESULT_OK;
+}
+
+void AirGradient::MHZ19Sensor::getData(AirGradient::SensorData &data) const {
+    data.GAS_DATA.CO2 = _sensor->getCO2();
+}
---
+++ src/Sensors/CO2/MHZ19Sensor.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <SoftwareSerial.h>
+#include "Sensors/ISensor.h"
+#include "MHZ19.h"
+
+#define WINSEN_MH_Z19_DEFAULT_BAUDRATE 9600
+
+
+namespace AirGradient {
+    class MHZ19Sensor : public ISensor {
+    public:
+        const char *getName() const override {
+            return "Winsen MH-Z19";
+        }
+
+        SensorType getType() const override {
+            return SensorType::CO2;
+        }
+
+        MHZ19Sensor(uint8_t rxPin, uint8_t txPin, uint16_t baudRate) : _rxPin(rxPin),
+                                                                       _txPin(txPin),
+                                                                       _baudRate(baudRate) {}
+
+        MHZ19Sensor() : MHZ19Sensor(D4, D3, WINSEN_MH_Z19_DEFAULT_BAUDRATE) {}
+
+        bool begin() override;
+
+        void getData(SensorData &data) const override;
+
+    private:
+        std::unique_ptr<SoftwareSerial> _softwareSerial;
+        std::unique_ptr<MHZ19> _sensor;
+        uint8_t _rxPin;
+        uint8_t _txPin;
+        uint16_t _baudRate;
+    };
+}
+
