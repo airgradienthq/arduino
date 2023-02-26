ef802593a6d63bf52feb6ede08e64bd6276474f0
Achim <achim.haug@airgradient.com>
2023-02-26 16:57:08 +0700

    Updated default display orientation

--- examples/DIY_PRO_V3_7/DIY_PRO_V3_7.ino
+++ examples/DIY_PRO_V3_7/DIY_PRO_V3_7.ino
@@ -56,10 +56,10 @@ int addr = 0;
 byte value;
 
 // Display bottom right
-//U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
 // Replace above if you have display on top left
-U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R2, /* reset=*/ U8X8_PIN_NONE);
+//U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R2, /* reset=*/ U8X8_PIN_NONE);
 
 
 // CONFIGURATION START
