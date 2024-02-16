2941bb2d5d29325e4d6daeb8bf0080d401b78ad8
Phat Nguyen <phat.nt@hotmail.com>
2024-02-16 21:56:43 +0700

    next version `3.0.3`

--- src/AirGradient.cpp
+++ src/AirGradient.cpp
@@ -1,6 +1,6 @@
 #include "AirGradient.h"
 
-#define AG_LIB_VER "3.0.2"
+#define AG_LIB_VER "3.0.3"
 
 AirGradient::AirGradient(BoardType type)
     : pms5003(type), pms5003t_1(type), pms5003t_2(type), s8(type), sgp41(type),
