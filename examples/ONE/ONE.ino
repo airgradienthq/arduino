airgradienthq/arduino

commit e47096feac33ca83000c85e7dab289173dc1e904

Limit show CO2 index within 4 character number (max = 9999)

examples/ONE/ONE.ino
@@ -1453,6 +1453,10 @@ static void displayShowDashboard(String err) {
     /** Show CO2 value */
     u8g2.setFont(u8g2_font_t0_22b_tf);
     if (co2Ppm > 0) {
+      int val = 9999;
+      if (co2Ppm < 10000) {
+        val = co2Ppm;
+      }
       sprintf(strBuf, "%d", co2Ppm);
     } else {
       sprintf(strBuf, "%s", "-");

examples/OneOpenAir/OneOpenAir.ino
@@ -1656,7 +1656,11 @@ static void displayShowDashboard(String err) {
     /** Show CO2 value */
     u8g2.setFont(u8g2_font_t0_22b_tf);
     if (co2Ppm > 0) {
-      sprintf(strBuf, "%d", co2Ppm);
+      int val = 9999;
+      if(co2Ppm < 10000){
+        val = co2Ppm;
+      }
+      sprintf(strBuf, "%d", val);
     } else {
       sprintf(strBuf, "%s", "-");
     }

commit a643ee31282c8921a82ef35736e1cdb8d50ba6f0

Use capped 'val' value in displayShowDashboard()
Fixes: e47096feac33 ("Limit show CO2 index within 4 character number (max = 9999)")

examples/ONE/ONE.ino
@@ -1457,7 +1457,7 @@ static void displayShowDashboard(String err) {
       if (co2Ppm < 10000) {
         val = co2Ppm;
       }
-      sprintf(strBuf, "%d", co2Ppm);
+      sprintf(strBuf, "%d", val);
     } else {
       sprintf(strBuf, "%s", "-");
     }