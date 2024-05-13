7a4255b2bb1fa9067cef56b352865889fd54c77d
Phat Nguyen <phat.nt@hotmail.com>
2024-05-13 10:34:06 +0700

    Turn off LED bar and Display if brightness is 0%, fix #114

--- src/AgOledDisplay.h
+++ src/AgOledDisplay.h
@@ -14,6 +14,7 @@ private:
   bool isBegin = false;
   void *u8g2 = NULL;
   Measurements &value;
+  bool isDisplayOff = false;
 
   void showTempHum(bool hasStatus);
   void setCentralText(int y, String text);
