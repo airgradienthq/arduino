244b7814a6b64018f8abee2e30e0dd84d3a771dc
Phat Nguyen <phat.nt@hotmail.com>
2024-08-30 19:20:08 +0700

    add link to formula documents

--- src/PMS/PMS5003TBase.cpp
+++ src/PMS/PMS5003TBase.cpp
@@ -4,6 +4,14 @@ PMS5003TBase::PMS5003TBase() {}
 
 PMS5003TBase::~PMS5003TBase() {}
 
+/**
+ * @brief Compensate the temperature
+ * 
+ * Reference formula: https://www.airgradient.com/documentation/correction-algorithms/
+ * 
+ * @param temp 
+ * @return * float 
+ */
 float PMS5003TBase::compensateTemp(float temp) {
   if (temp < 10.0f) {
     return temp * 1.327f - 6.738f;
@@ -11,6 +19,14 @@ float PMS5003TBase::compensateTemp(float temp) {
   return temp * 1.181f - 5.113f;
 }
 
+/**
+ * @brief Compensate the humidity
+ * 
+ * Reference formula: https://www.airgradient.com/documentation/correction-algorithms/
+ * 
+ * @param temp 
+ * @return * float 
+ */
 float PMS5003TBase::compensateHum(float hum) {
   hum = hum * 1.259f + 7.34f;
 
