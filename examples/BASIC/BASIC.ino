77a23b4202bc5458ed51f80a5957be491d176631
Samuel Siburian <samuelbles07@gmail.com>
2024-10-13 15:46:41 +0700

    Always increment bootcount when send measurements data is scheduled (#255)

--- examples/BASIC/BASIC.ino
+++ examples/BASIC/BASIC.ino
@@ -534,6 +534,9 @@ static void updatePm(void) {
 }
 
 static void sendDataToServer(void) {
+  /** Increment bootcount when send measurements data is scheduled */
+  measurements.bootCount++;
+
   /** Ignore send data to server if postToAirGradient disabled */
   if (configuration.isPostDataToAirGradient() == false ||
       configuration.isOfflineMode()) {
@@ -548,7 +551,6 @@ static void sendDataToServer(void) {
         "Online mode and isPostToAirGradient = true: watchdog reset");
     Serial.println();
   }
-  measurements.bootCount++;
 }
 
 static void tempHumUpdate(void) {
