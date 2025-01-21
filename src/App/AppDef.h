e9b27185b42f99bf0ce436c9f904415d6695f3fd
samuelbles07 <samuelbles07@gmail.com>
2025-01-22 01:46:08 +0700

    Fix spaces

--- src/App/AppDef.h
+++ src/App/AppDef.h
@@ -107,14 +107,14 @@ enum PMCorrectionAlgorithm {
 };
 
 enum AgFirmwareMode {
-  FW_MODE_I_9PSL, /** ONE_INDOOR */
-  FW_MODE_O_1PST, /** PMS5003T, S8 and SGP41 */
-  FW_MODE_O_1PPT, /** PMS5003T_1, PMS5003T_2, SGP41 */
-  FW_MODE_O_1PP,  /** PMS5003T_1, PMS5003T_2 */
-  FW_MODE_O_1PS,  /** PMS5003T, S8 */
-  FW_MODE_O_1P,   /** PMS5003T */
-  FW_MODE_I_42PS, /** DIY_PRO 4.2 */
-  FW_MODE_I_33PS, /** DIY_PRO 3.3 */
+  FW_MODE_I_9PSL,       /** ONE_INDOOR */
+  FW_MODE_O_1PST,       /** PMS5003T, S8 and SGP41 */
+  FW_MODE_O_1PPT,       /** PMS5003T_1, PMS5003T_2, SGP41 */
+  FW_MODE_O_1PP,        /** PMS5003T_1, PMS5003T_2 */
+  FW_MODE_O_1PS,        /** PMS5003T, S8 */
+  FW_MODE_O_1P,         /** PMS5003T */
+  FW_MODE_I_42PS,       /** DIY_PRO 4.2 */
+  FW_MODE_I_33PS,       /** DIY_PRO 3.3 */
   FW_MODE_I_BASIC_40PS, /** DIY_BASIC 4.0 */
 };
 const char *AgFirmwareModeName(AgFirmwareMode mode);
