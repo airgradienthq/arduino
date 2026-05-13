2fd53ac303d78ab7ae8674648134d2abe6a14878
samuelbles07 <samuelbles07@gmail.com>
2026-05-13 12:43:29 +0500

    Remove BLE write logs

--- src/AgWiFiConnector.cpp
+++ src/AgWiFiConnector.cpp
@@ -846,6 +846,8 @@ void WifiConnector::CharacteristicCallbacks::onRead(NimBLECharacteristic *pChara
 }
 
 void WifiConnector::CharacteristicCallbacks::onWrite(NimBLECharacteristic *pCharacteristic, NimBLEConnInfo &connInfo) {
+  Serial.printf("%s : onWrite()\n", pCharacteristic->getUUID().toString().c_str());
+
   auto bleCred = NimBLEUUID(BLE_CRED_CHAR_UUID);
   if (pCharacteristic->getUUID().equals(bleCred)) {
     if (!parent->wifiConnecting) {
